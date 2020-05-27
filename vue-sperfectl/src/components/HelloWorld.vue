094a633b7e3ae506e522aa06571144be792d45b0
sunying128 <284673528@qq.com>
2020-05-27 17:50:45 +0800

    首页

--- vue-sperfectl/src/components/HelloWorld.vue
+++ vue-sperfectl/src/components/HelloWorld.vue
@@ -1,113 +1,104 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <header>
+      <div class="logoimg">
+        <img :src="logoImg" height="42px" />
+      </div>
+      <div class="headerNav">
+        <el-row>
+          <el-button icon="el-icon-search" type="text"></el-button>
+          <el-button icon="el-icon-edit" type="text"></el-button>
+          <el-button icon="el-icon-check" type="text"></el-button>
+          <el-button icon="el-icon-message" type="text"></el-button>
+          <el-button type="text">登录</el-button>
+        </el-row>
+      </div>
+    </header>
+    <nav class="navBackground">
+      <div class="navImage">
+        <img :src="navimg" height="700px" />
+      </div>
+
+      <div class="navText">
+        <!-- <img :src="navimg" height="700px" /> -->
+        <div class="navwriting">记录点滴生活  |  记录点滴生活
+          <br>记录点滴生活  |  记录点滴生活
+          <br>记录点滴生活  |  记录点滴生活
+          <br>记录点滴生活  |  记录点滴生活
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      logoImg: require("../assets/img/logo.png"),
+      navimg: require("../assets/img/header.png")
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+header {
+  padding: 10px 0;
+  height: 42px;
+  display: flex;
+  justify-content: space-around;
+  overflow: hidden;
+  border-bottom: 1px solid rgb(209, 209, 209);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.logoimg {
+  line-height: 42px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.el-button--text {
+  line-height: 14px;
+  color: rgb(126, 127, 128);
 }
-a {
-  color: #42b983;
+header .headerNav {
+  /* line-height: 20px; */
+  align-content: center;
+}
+nav.navBackground {
+  width: 100%;
+  /* padding-top: 50px; */
+  height: 800px;
+  background: url("../assets/img/headerbackground.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+}
+
+.navBackground .navImage {
+  /* height: 700px; */
+  padding-top: 100px;
+  align-items: flex-end;
+  flex: 1;
+  -webkit-box-flex: 1;
+  padding-left: 200px;
+  overflow: hidden;
+}
+
+.navBackground .navText {
+  /* height: 500px; */
+  /* border: 1px solid #999; */
+  flex: 1;
+  -webkit-box-flex: 1;
+  margin-right: 400px;
+}
+.navwriting {
+  /* display: flex;
+  justify-content: center;
+  align-items:center; */
+  font-size: 40px;
+  margin-top: 300px;
+  color: #666;
+  /* border: 1px solid #999; */
+
 }
 </style>
