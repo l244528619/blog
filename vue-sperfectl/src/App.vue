1b6c9ec8b7ba5278550d9dd5ec5e0510df5ff28c
sunying128 <284673528@qq.com>
2020-05-29 17:41:30 +0800

    login页面

--- vue-sperfectl/src/App.vue
+++ vue-sperfectl/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
+}
+html,
+body,
+#app {
+  height: 100%;
 }
 /* #app {
   margin: 0;
