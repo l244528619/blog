1962de44ff8b748b6d247f3a1b40f7d20c588d24
sunying128 <284673528@qq.com>
2020-06-03 15:08:40 +0800

    ddd

--- vue-sperfectl/src/components/Home/home.vue
+++ vue-sperfectl/src/components/Home/home.vue
@@ -16,7 +16,7 @@
           >
             <el-menu-item index="1">
               <i class="el-icon-location"></i>
-              <span slot="title">导航一</span>
+              <span slot="title">权限管理</span>
             </el-menu-item>
             <el-menu-item index="2">
               <i class="el-icon-menu"></i>
